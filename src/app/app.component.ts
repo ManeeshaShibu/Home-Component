b27a40171199942c807e4a5314ce36453bc802d9
ManeeshaShibu <maneesha.shibu2@gmail.com>
2024-02-14 10:13:35 +0530

    Home-component : This is a sample demo just for learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component'; 
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <main>
+    <header class="brand-name">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <i class="fas fa-home">Home</i>
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`
 })
 export class AppComponent {
   title = 'Home-component';
